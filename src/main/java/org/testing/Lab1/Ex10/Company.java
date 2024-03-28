fcbc3f981d2ef4bd02bcac60c1161e15780594c5
Skerdian Shehaj <skerdians@cdata.com>
2024-03-28 14:57:19 -0400

    First commit for Lab 1 without the last Ex

---
+++ src/main/java/org/testing/Lab1/Ex10/Company.java
@@ -0,0 +1,6 @@
+package org.testing.Lab1.Ex10;
+
+public class Company {
+  private String companyName;
+  private Employee[] employees;
+}
---
+++ src/main/java/org/testing/Lab1/Ex10/Main.java
@@ -0,0 +1,28 @@
+package org.testing.Lab1.Ex10;
+
+/**
+ * Implement a class named Employee, which consists of three fields:
+ * firstName, lastName and age.
+ * Add a three-argument constructor, which allows initializing field values and getter for age field.
+ *
+ * In the next step, please create class named Company, which consists of the fields:
+ * name of String type - company name
+ * employees, which is a 5-element table of Employee type (Employee[]) - table will store all company employees
+ * <p>
+ * In class Company, please also add one-argument constructor, which takes name parameter as an argument,
+ * create setEmployees() method definition for setting company employees.
+ * Implement toString() methods for every class (Employee and Company,
+ * use Alt+Insert -> toString in IntelliJ), which will be returning whole object data.​
+ * <p>
+ * Implement also getAverageAge(), which returns the average age of company employees.
+ * <p>
+ * In the last step, please create a test class named CompanyTest.
+ * In the main() method, create 5 instances of Employee type,
+ * which will be a representation of 5 employees working in the company.
+ * Then add them to the table and create an instance of type Company using one-argument constructor. ​
+ * <p>
+ * Set employees field values using previously created setter.
+ * Next, please display all company data on the standard output (hint: toString() methods will be helpful) and average age computed by calling getAverageAge() method on Company type instance.
+ */
+public class Main {
+}
